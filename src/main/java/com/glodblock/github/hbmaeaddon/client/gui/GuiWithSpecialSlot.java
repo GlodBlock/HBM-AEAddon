978278197cf913527648ab1fe3adf33e81838820
GlodBlock <1356392126@qq.com>
2024-10-13 18:21:18 +0800

    nei

--- src/main/java/com/glodblock/github/hbmaeaddon/client/gui/GuiWithSpecialSlot.java
+++ src/main/java/com/glodblock/github/hbmaeaddon/client/gui/GuiWithSpecialSlot.java
@@ -1,6 +1,6 @@
 package com.glodblock.github.hbmaeaddon.client.gui;
 
-import java.awt.*;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
